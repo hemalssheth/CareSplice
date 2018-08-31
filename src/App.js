2b3bd051f6a236e3ed4d6b4452399cf03d96510a
hemalssheth <42876235+hemalssheth@users.noreply.github.com>
2018-09-01 00:34:48 +0530

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React Shashank</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload1_after clone.
+          bhad ma ja
         </p>
       </div>
     );
